6714f12d3b07f5b1fd25fa44e0299e7c2ea0c704
sadiaimran <sadiaimranmalik24@gmail.com>
2025-01-27 21:17:35 +0500

    done

--- hacathon/src/app/layout.tsx
+++ hacathon/src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 import { CartProvider } from "@/context/CartContext";
 
 const geistSans = Geist({
@@ -25,7 +27,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    </CartProvider>
+    <CartProvider>
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
